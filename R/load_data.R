26568cba20c9404e4e933d109a973e9fb089000a
DylanCTY <u5504008@live.warwick.ac.uk>
2024-03-10 01:26:43 +0000

    Added database and Loaded data

---
+++ R/load_data.R
@@ -0,0 +1,6 @@
+library(readr)
+library(RSQLite)
+
+mock_data <- readr::read_csv("data_upload/mock_data.csv")
+my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"database/database.db")
+RSQLite::dbWriteTable(my_connection, "clients", mock_data)
