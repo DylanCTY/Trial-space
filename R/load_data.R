8ba768ea0d5645091d0d45634b826e93ab49ad1b
DylanCTY <u5504008@live.warwick.ac.uk>
2024-03-16 17:33:26 +0000

    Update overwrite and append

--- R/load_data.R
+++ R/load_data.R
@@ -3,4 +3,4 @@ library(RSQLite)
 
 mock_data <- readr::read_csv("data_upload/mock_data.csv")
 my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"database/database.db")
-RSQLite::dbWriteTable(my_connection, "clients", mock_data)
+RSQLite::dbWriteTable(my_connection, "clients", mock_data, overwrite = TRUE, append = FALSE)
